cc92f7645b94336c73ff842aec4d6fb74ba79c67
ian <ian@feedmob.com>
2019-06-02 17:20:18 +0900

    first version of s3 upload

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"what is your name"`
-	Age  int    `json:"how old are you"`
-}
+// type MyEvent struct {
+// 	Name string `json:"what is your name"`
+// 	Age  int    `json:"how old are you"`
+// }
 
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
+// type MyResponse struct {
+// 	Message string `json:"Result:"`
+// }
 
-func osakaHandler(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d old!", event.Name, event.Age)}, nil
+func osakaHandler(ctx context.Context, event events.S3Event) {
+	for _, record := range event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+	}
 }
 
 //How to build go osaka
